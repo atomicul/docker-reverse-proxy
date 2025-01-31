77f10e77ea532a0353a9617e2a88bb7dd242fbb3
atomicul <soaremihai2006@gmail.com>
2025-01-31 18:28:52 +0200

    v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 # v0.1.0
 * Add Dockerfile and entrypoint
 * Add github actions to release and publish docker images
+
+# v0.1.1
+* Fix a dns issue when used in docker compose
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+line_number=$(
+    grep -m 1 -nE '^http\s*{' < /etc/nginx/nginx.conf |
+    awk -F ':' '{print $1}'
+)
+
+sed -i "$line_number a resolver 127.0.0.11;" /etc/nginx/nginx.conf
+
 cat >/etc/nginx/sites-enabled/default <<'EOF'
 server {
     listen 80;
@@ -15,6 +22,8 @@ do
     cat >>/etc/nginx/sites-enabled/default <<EOF
     location $prefix {
         rewrite ^$prefix(.*) /\$1  break;
+        # this set prevents some dns caching issues when used with docker-compose
+        # set \$target $host\$uri\$is_args\$args;
         proxy_pass $host\$uri\$is_args\$args;
     }
 EOF
